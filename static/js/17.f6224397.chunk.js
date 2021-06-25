9c1b454bfadc7f51d509b194cc9fe9e8a69d8d9a
venkatAlapati123 <69452390+venkatAlapati123@users.noreply.github.com>
2021-06-25 17:15:27 +0530

    Updates

---
+++ static/js/17.f6224397.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpamplify-project"]=this["webpackJsonpamplify-project"]||[]).push([[17],{365:function(t,e,n){"use strict";n.r(e),n.d(e,"amplify_auth_container",(function(){return p}));var i=n(9),p=function(){function t(t){Object(i.k)(this,t)}return t.prototype.render=function(){return Object(i.i)(i.b,null,Object(i.i)("form",{autoComplete:"on",hidden:!0},Object(i.i)("input",{name:"username"}),Object(i.i)("input",{name:"password",type:"password"}),Object(i.i)("input",{type:"submit"})),Object(i.i)("slot",null))},t}()}}]);
+//# sourceMappingURL=17.f6224397.chunk.js.map
